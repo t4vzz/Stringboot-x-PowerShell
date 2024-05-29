c6ad3c05aa7f526830370348366eef497abf7185
t4vzz <souzza.otavio@gmail.com>
2024-05-28 22:28:06 -0300

    StringBoot com PowerShell

---
+++ src/main/java/br/comsuaempresa/estudandostring/EstudandostringApplication.java
@@ -0,0 +1,13 @@
+package br.comsuaempresa.estudandostring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudandostringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudandostringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/comsuaempresa/estudandostring/EstudandostringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.comsuaempresa.estudandostring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudandostringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
